cd2f8d4c55feaa84eb572ca15f51ba6bd1f8d97e
TitasStr <titas.strasunskas@gmail.com>
2024-02-07 22:04:14 +0200

    chore: Applied linting and formatting rules with ESLint and Prettier

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { Movie } from './movie.entity';
+import { MovieService } from './movie.service';
 
 @Controller('movie')
-export class MovieController {}
+export class MovieController {
+  constructor(private movieService: MovieService) {}
+  @Get('search')
+  search(@Query('term') searchTerm: string): Promise<Movie[]> {
+    return this.movieService.searchMovies(searchTerm);
+  }
+}
